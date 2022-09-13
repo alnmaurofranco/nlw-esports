e86f6079335945f42c8279c7d2605d8181df9d00
alnmaurofranco <jefeson23@live.com>
2022-09-13 17:32:09 -0300

    ✨ feat: tailwind configuration

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx", "./index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)",
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
+        galaxy: 'url("./background-galaxy.png")',
+      },
+    },
+  },
+  plugins: [],
+};
