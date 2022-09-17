400c9795ca4adecd33f8bfa9bdf94317f5d4ca6b
alnmaurofranco <jefeson23@live.com>
2022-09-16 21:06:12 -0300

    chore: add env in gitignore and new style tailwind css

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -11,6 +11,8 @@ module.exports = {
           "linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)",
         "game-gradient":
           "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
+        "game-gradient-2":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 40.08%)",
         galaxy: 'url("./background-galaxy.png")',
       },
     },
