089c5bc8ca43088fc4e73ee333574966680ca508
alnmaurofranco <jefeson23@live.com>
2022-09-27 16:53:20 -0300

    ♻️ refactor: change http client

--- server/src/infra/adapter/http-client/http-client.ts
+++ server/src/infra/adapter/http-client/http-client.ts
@@ -1,4 +1,4 @@
 export default interface HttpClient {
   get<T>(url: string, options?: any): Promise<T>;
-  post<T>(url: string, data: any, options?: any): Promise<T>;
+  post<T>(url: string, data?: any, options?: any): Promise<T>;
 }
