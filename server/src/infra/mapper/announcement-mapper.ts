85b1b25fd90ecba68c5fa6fc59278d643dd06a6f
alnmaurofranco <jefeson23@live.com>
2022-09-27 16:38:33 -0300

    ✨ feat: add mapper

---
+++ server/src/infra/mapper/announcement-mapper.ts
@@ -0,0 +1,40 @@
+import { Announcement as AnnouncementPersistence } from "@prisma/client";
+import Announcement from "../../domain/entity/announcement";
+
+export default class AnnouncementMapper {
+  static toDomain(announcement: AnnouncementPersistence): Announcement {
+    return new Announcement(
+      {
+        name: announcement.name,
+        yearPlaying: announcement.yearPlaying,
+        weekDays: announcement.weekDays as { days: number[] },
+        hourStart: announcement.hourStart,
+        hourEnd: announcement.hourEnd,
+        useVoiceChannel: announcement.useVoiceChannel,
+        discord: announcement.discord,
+        gameId: announcement.gameId,
+        createdAt: announcement.createdAt,
+      },
+      announcement.id
+    );
+  }
+
+  toPersistence(announcement: Announcement): AnnouncementPersistence {
+    return {
+      id: announcement.id,
+      name: announcement.name,
+      yearPlaying: announcement.yearPlaying,
+      weekDays: announcement.weekDays,
+      hourStart: announcement.hourStart,
+      hourEnd: announcement.hourEnd,
+      useVoiceChannel: announcement.useVoiceChannel,
+      discord: announcement.discord,
+      gameId: announcement.gameId,
+      createdAt: announcement.createdAt ?? new Date(),
+    };
+  }
+
+  static toDTO<T>(announcement: T): Announcement {
+    return announcement as Announcement;
+  }
+}
---
+++ server/src/infra/mapper/game-mapper.ts
@@ -0,0 +1,41 @@
+import {
+  Game as GamePrisma,
+  Announcement as AnnouncementPrisma,
+  Prisma,
+} from "@prisma/client";
+import Announcement from "../../domain/entity/announcement";
+import { GameWithRelationAnnoucementResult } from "../../domain/repository/game-repository";
+
+type GamePersistence = GamePrisma & {
+  announcements: AnnouncementPrisma[];
+  _count: Prisma.GameCountOutputType;
+};
+
+export default class GameMapper {
+  static toDTO(raw: GamePersistence): GameWithRelationAnnoucementResult {
+    return {
+      id: raw.id,
+      title: raw.title,
+      bannerURL: raw.bannerURL,
+      announcements: raw.announcements.map((announcement) =>
+        new Announcement(
+          {
+            name: announcement.name,
+            weekDays: announcement.weekDays as {
+              days: number[];
+            },
+            useVoiceChannel: announcement.useVoiceChannel,
+            hourStart: announcement.hourStart!,
+            hourEnd: announcement.hourEnd!,
+            yearPlaying: announcement.yearPlaying,
+            discord: announcement.discord,
+            gameId: announcement.gameId,
+            createdAt: announcement.createdAt,
+          },
+          announcement.id
+        ).getJSON()
+      ),
+      count: raw._count.announcements,
+    };
+  }
+}
