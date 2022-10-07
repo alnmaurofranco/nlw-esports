616c9514538cb6ad22cd6ec65f14605ca1ddf42c
alnmaurofranco <jefeson23@live.com>
2022-10-07 16:28:47 -0300

    chore: remove comments

--- server/src/infra/factory/authenticate-email-factory.ts
+++ server/src/infra/factory/authenticate-email-factory.ts
@@ -1,9 +1,14 @@
 import AuthenticateEmailUseCase from "../../application/use-cases/authenticate-email/authenticate-email-use-case";
 import AuthenticateEmailController from "../controller/authenticate-email-controller";
+import JwtTokenManager from "../manager/jwt-token-manager";
 import NodemailerMailProviderAdapter from "../provider/mail-provider/nodemailer-mail-provider-adapter";
 
 export default function AuthenticateEmailFactory(): AuthenticateEmailController {
   const mailProvider = new NodemailerMailProviderAdapter();
-  const authenticateEmailUseCase = new AuthenticateEmailUseCase(mailProvider);
+  const tokenManager = new JwtTokenManager();
+  const authenticateEmailUseCase = new AuthenticateEmailUseCase(
+    mailProvider,
+    tokenManager
+  );
   return new AuthenticateEmailController(authenticateEmailUseCase);
 }
